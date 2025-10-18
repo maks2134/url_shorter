4de31d7dfe7060cac1c6c6f6008e40d7e0076e62
maks2134 <maks210306@yandex.by>
2025-10-18 18:49:57 +0300

    create link handler signature

---
+++ internal/link/handler.go
@@ -0,0 +1,42 @@
+package link
+
+import (
+	"fmt"
+	"net/http"
+	"shorter-url/configs"
+)
+
+type LinkHandlerDeps struct {
+	*configs.Config
+}
+
+type LinkHandler struct {
+	*configs.Config
+}
+
+func NewLinkHandler(router *http.ServeMux, deps *LinkHandlerDeps) {
+	linkHandler := &LinkHandler{
+		Config: deps.Config,
+	}
+
+	router.HandleFunc("GET /{alias}", linkHandler.CreateLink)
+	router.HandleFunc("POST /link", linkHandler.GetLink)
+	router.HandleFunc("PATCH /link/{id}", linkHandler.UpdateLink)
+	router.HandleFunc("DELETE /link/{id}", linkHandler.DeleteLink)
+}
+
+func (link *LinkHandler) CreateLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create Link")
+}
+
+func (link *LinkHandler) GetLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get Link")
+}
+
+func (link *LinkHandler) DeleteLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete Link")
+}
+
+func (link *LinkHandler) UpdateLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update Link")
+}
