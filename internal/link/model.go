bfc9985bce8538fec02439da60083820879447db
maks2134 <maks210306@yandex.by>
2025-10-23 16:05:22 +0300

    new cash generate

--- internal/link/model.go
+++ internal/link/model.go
@@ -1,10 +1,16 @@
 package link
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math/big"
 	"shorter-url/internal/stat"
 
 	"gorm.io/gorm"
+)
+
+const (
+	hashLength  = 10
+	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
 type Link struct {
@@ -18,22 +24,30 @@ func NewLink(url string) *Link {
 	link := &Link{
 		Url: url,
 	}
-	link.generateHash()
-
+	err := link.generateHash()
+	if err != nil {
+		panic("failed to generate hash: " + err.Error())
+	}
 	return link
 }
 
-func (link *Link) generateHash() {
-	link.Hash = RandStingRunes(10)
+func (link *Link) generateHash() error {
+	hash, err := generateRandomHash(hashLength)
+	if err != nil {
+		return err
+	}
+	link.Hash = hash
+	return nil
 }
 
-var letterRunes = []rune("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
-
-func RandStingRunes(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+func generateRandomHash(n int) (string, error) {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterBytes))))
+		if err != nil {
+			return "", err
+		}
+		b[i] = letterBytes[num.Int64()]
 	}
-
-	return string(b)
+	return string(b), nil
 }
